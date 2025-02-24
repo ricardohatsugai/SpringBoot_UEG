59b70631be25eb211ec4c7d3ef5c07285752cf53
Natallya-BS <natallya.estudos@gmail.com>
2025-02-24 19:34:42 -0300

    firstCommit

---
+++ projeto-JavaRicardo/src/main/java/br/ueg/trindade/desenvolvimento/projeto_JavaRicardo/ProjetoJavaRicardoApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.desenvolvimento.projeto_JavaRicardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJavaRicardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJavaRicardoApplication.class, args);
+	}
+
+}
---
+++ projeto-JavaRicardo/src/test/java/br/ueg/trindade/desenvolvimento/projeto_JavaRicardo/ProjetoJavaRicardoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.desenvolvimento.projeto_JavaRicardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaRicardoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
